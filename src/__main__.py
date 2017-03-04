2fbe32d461f3c956edb36595b8db934c2805a395
Carlos González Cebrecos <c.cebrecos@hotmail.com>
2017-03-04 13:01:28 +0100

    Pylama style convention corrected

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""
+Main file to execute, contains all the stuff to start the application properly
+"""
 
-#Libraries
+# Libraries
 import os
 import sys
 import platform
@@ -11,7 +14,8 @@ import core.log
 from cli.arguments.parser import DEFAULT_PARSER
 from cli.arguments.constants import *
 
-#Constants
+
+# Constants
 LOGGER = logging.getLogger(__name__)
 
 # variables
@@ -20,27 +24,32 @@ Arguments namespace
 """
 args = None
 
-#Functions
-"""
-Takes the system arguments vector and tries to parse the arguments in it given
-the argument parser specified and returns the namespace generated
 
-@param 	parser 	the ArgumentParser objects to use to parse the arguments
-@param 	args 	arguments to parse (default is sys.argv)
-@return namespace of parsed arguments
-"""
+# Functions
 def parseArguments(parser, args=None):
-	return parser.parse_args(args)
+    """
+    Takes the system arguments vector and tries to parse the arguments in it
+    given the argument parser specified and returns the namespace generated
+
+    Args:
+        parser(): the ArgumentParser objects to use to parse the arguments
+        args(): arguments to parse (default is sys.argv)
+
+    Returns:
+        namespace of parsed arguments
+    """
+    return parser.parse_args(args)
+
 
 if __name__ == "__main__":
-	# Prepare coding
-	if platform.system() == "Windows":
-		os.system("chcp 65001")
-	args = parseArguments(DEFAULT_PARSER)
-	# Switching log level
-	root_logger = logging.getLogger()
-	root_logger.setLevel(LOGS_LEVELS[LOGS.index(args.log_level)])
-	#Welcome
-	LOGGER.info("Welcome !")
-	#Exiting
-	LOGGER.info("Bye !")
+    # Prepare coding
+    if platform.system() == "Windows":
+        os.system("chcp 65001")
+    args = parseArguments(DEFAULT_PARSER)
+    # Switching log level
+    root_logger = logging.getLogger()
+    root_logger.setLevel(LOGS_LEVELS[LOGS.index(args.log_level)])
+    # Welcome
+    LOGGER.info("Welcome !")
+    # Exiting
+    LOGGER.info("Bye !")
